47143515ba055c6d31a5c686360cc5eed277d044
Todd Fredrich <tfredrich@pingidentity.com>
2017-06-29 08:40:14 -0600

    Added Reference (interface) and ReferenceDefinition (implementation).

---
+++ core/src/main/java/com/strategicgains/hyperexpress/domain/ReferenceDefinition.java
@@ -0,0 +1,31 @@
+package com.strategicgains.hyperexpress.domain;
+
+public class ReferenceDefinition<T>
+implements Reference<T>
+{
+	private T id;
+
+	public ReferenceDefinition(T id)
+	{
+		super();
+		setId(id);
+	}
+
+	@Override
+	public ReferenceDefinition<T> clone()
+	{
+		return null;
+	}
+
+	@Override
+	public T getId()
+	{
+		return id;
+	}
+
+	@Override
+	public void setId(T id)
+	{
+		this.id = id;
+	}
+}
